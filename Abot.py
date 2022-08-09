43d8f4db40e7c6e7d32013e0a60c16b24dff2161
Black0777 <87493630+Black0777@users.noreply.github.com>
2022-08-09 18:43:42 +0530

    Update Abot.py

--- Abot.py
+++ Abot.py
@@ -37,7 +37,7 @@ path_utilities = "utilities"
 path_download = "audio"
 path_lock = f"{path_utilities}/locked"
 
-client = BotAmino("oaz7zgj9oa@vddaz.com","indian4321")
+client = BotAmino("kaifansari1819@gmail.com","KAIFANSARI(+-1")
 client.prefix = "/"
 
 
